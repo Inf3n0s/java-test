8c873975b9dd34d61727179b86ac0bdbcab3a06b
Scânteie Alexandru <alexscanteie0808@gmail.com>
2022-11-02 16:41:50 +0200

    aa

--- tema1.java
+++ tema1.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class tema1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+            sum += numbers[i];
+        }
+        System.out.print(sum);
+    }
+}
